20d8c24d64e1c1c2982f726a07a95aac69a87c0f
Adhikram <adhikramm@gmail.com>
2020-08-05 07:34:08 +0530

    Second  Commit

---
+++ build/precache-manifest.664f322e452b1133a3ef05b9bf4214f4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1d6fe67da93943b925f",
+    "url": "/React_Portfolio/static/css/main.efcc45ed.chunk.css"
+  },
+  {
+    "revision": "c1d6fe67da93943b925f",
+    "url": "/React_Portfolio/static/js/main.c1d6fe67.chunk.js"
+  },
+  {
+    "revision": "3bbdfe8d132fce374b98",
+    "url": "/React_Portfolio/static/css/1.e6b8d714.chunk.css"
+  },
+  {
+    "revision": "3bbdfe8d132fce374b98",
+    "url": "/React_Portfolio/static/js/1.3bbdfe8d.chunk.js"
+  },
+  {
+    "revision": "0b3b1d1736f2c5113254",
+    "url": "/React_Portfolio/static/js/runtime~main.0b3b1d17.js"
+  },
+  {
+    "revision": "acff6ce411cd8c6ce6798b88e15ac828",
+    "url": "/React_Portfolio/static/media/DSC02518-min.acff6ce4.jpg"
+  },
+  {
+    "revision": "45da1b4ceb15f42c8f638fa50f463328",
+    "url": "/React_Portfolio/static/media/IMG_20191012_124520-min.45da1b4c.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/React_Portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/React_Portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/React_Portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/React_Portfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a70da5ea747bfc05d8cbe62e8562b43d",
+    "url": "/React_Portfolio/static/media/unnamed.a70da5ea.png"
+  },
+  {
+    "revision": "1d92a789d863b5e62427861a49c76245",
+    "url": "/React_Portfolio/static/media/adscope-hero.1d92a789.jpg"
+  },
+  {
+    "revision": "2d51219f5480507902ddc3c21e5d26ac",
+    "url": "/React_Portfolio/static/media/KBL.2d51219f.jpg"
+  },
+  {
+    "revision": "2cfcf00f4a8544a4c7b34f1ade88ffc0",
+    "url": "/React_Portfolio/static/media/schoolforjustice-hero.2cfcf00f.jpg"
+  },
+  {
+    "revision": "eaafe86cb500e379277cce23ea5e3f04",
+    "url": "/React_Portfolio/static/media/shuttershare-hero.eaafe86c.jpg"
+  },
+  {
+    "revision": "e0dc56660e1177c8b8d09812dac3b3b5",
+    "url": "/React_Portfolio/index.html"
+  }
+];
